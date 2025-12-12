8467dd7f886f5132cb5b8f91d0fafdf2d041f5a0
Paras <paras451@gmail.com>
2025-12-12 09:39:49 +0530

    for render fix

---
+++ src/main/java/Backend/Custom_tech/controller/PingController.java
@@ -0,0 +1,14 @@
+package Backend.Custom_tech.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "alive";
+    }
+}
